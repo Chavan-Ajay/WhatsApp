b581a9e2776331da1cc978525b75e6c6db68f197
AjayChavan03 <ajpremiumyt@gmail.com>
2024-11-28 03:44:47 +0530

    first commit

---
+++ src/routers/messageRouter.js
@@ -0,0 +1,23 @@
+
+const express = require('express');
+const router = new express.Router();
+const whatsappClient = require("../services/WhatsappClient")
+const multer  = require('multer')
+const upload = multer()
+
+router.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+router.post("/message", (req, res) => {
+
+  whatsappClient.sendMessage(req.body.phoneNumber, req.body.message);
+  res.send();
+})
+
+// router.get('/:id/start', (req, res) => {
+//   startClient(req.params.id)
+//   res.send()
+// })
+
+module.exports = router
